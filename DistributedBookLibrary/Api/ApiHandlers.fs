0aacfc117e84c457d1fdf98e1a77cc349c5fbc6f
Margarita Krutikova <margaretkru@gmail.com>
2021-04-21 23:57:38 +0200

    Add get published listings api endpoint

--- DistributedBookLibrary/Api/ApiHandlers.fs
+++ DistributedBookLibrary/Api/ApiHandlers.fs
@@ -3,6 +3,7 @@ module Api.ApiHandlers
 open System
 open System.Threading.Tasks
 open Api.ApiModels
+open Api.Queries.Queries
 open Application.CommandHandler
 open Domain.Commands
 open Domain.Types
@@ -17,13 +18,22 @@ let private commandErrorToHttpResponse (next: HttpFunc) (ctx: HttpContext) (erro
     | Domain _ -> RequestErrors.BAD_REQUEST "" next ctx
     | Persistence _ -> RequestErrors.BAD_REQUEST "Please try again later" next ctx
 
-let private commandToHttpResponse (next: HttpFunc)
-                                  (ctx: HttpContext)
-                                  (result: Result<unit, CommandError>)
-                                  : Task<HttpContext option> =
+let private queryErrorToHttpResponse (next: HttpFunc) (ctx: HttpContext) (error: QueryError) =
+    match error with
+    | InternalError -> RequestErrors.BAD_REQUEST "Please try again later" next ctx
+    | RecordNotFound -> RequestErrors.NOT_FOUND "Object not found" next ctx
+
+let private commandToHttpResponse
+    (next: HttpFunc) (ctx: HttpContext) (result: Result<unit, CommandError>) : Task<HttpContext option> =
     match result with
     | Ok () -> json () next ctx
     | Error error -> commandErrorToHttpResponse next ctx error
+
+let private queryToHttpResponse
+    (next: HttpFunc) (ctx: HttpContext) (result: Result<'a, QueryError>) : Task<HttpContext option> =
+    match result with
+    | Ok data -> json data next ctx
+    | Error error -> queryErrorToHttpResponse next ctx error
 
 let handlePublishListing (next: HttpFunc) (ctx: HttpContext) =
     task {
@@ -42,4 +52,12 @@ let handlePublishListing (next: HttpFunc) (ctx: HttpContext) =
 
         let! result = compositionRoot.CommandHandler command
         return! commandToHttpResponse next ctx result
+    }
+
+let getPublishedListings (next: HttpFunc) (ctx: HttpContext) =
+    task {
+        let compositionRoot = ctx.GetService<CompositionRoot>()
+        let! result = compositionRoot.ReadStorage.getPublishedListings ()
+        
+        return! queryToHttpResponse next ctx result
     }
